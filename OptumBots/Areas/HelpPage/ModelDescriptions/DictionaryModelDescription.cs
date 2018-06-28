cbfff5dda1d70c1034bcf65f0daaf6e5e3118251
Jothi, Dwarakanath <JothiD@advisory.com>
2018-06-28 02:21:24 -0700

    Add project files.

---
+++ OptumBots/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace OptumBots.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ OptumBots/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace OptumBots.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
